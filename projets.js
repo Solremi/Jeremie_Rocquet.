78439a28ac0305f99003466d10147ea00c4c4540
Solremi <126828207+Solremi@users.noreply.github.com>
2023-12-02 17:43:39 +0100

    02.12.23

--- projets.js
+++ projets.js
@@ -1,2 +1,8 @@
-consol
+//je séléctionne et je stoke le cercle
+// Je crée une variable compteur initialisé à 0
+// j'ajoute un évènement au cercle
+// j'ajoute une action à compteur
+
+const btnRed = document.getElementById('btn-red')
+console.log(btnRed, "bouton rouge"); 
 	
